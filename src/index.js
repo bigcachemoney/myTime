0e17a3bb6f7209056421e859c1ca8c8fac2c2752
angrysoftwareengineer88@gmail.com <raylan.liang@gmail.com>
2020-12-12 17:41:45 -0500

    Edit file structure

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import "./styles/index.scss";
-
+import "./chart.js"
     // var data = [2, 4, 8, 10];
     // var pie = d3.pie()
     // console.log(pie(data))
 
 
-var slider = document.getElementById("ranger");
-var output = document.getElementById("value");
+// let slider = document.getElementById("ranger");
+// let output = document.getElementById("value");
 
-output.innerHTML = slider.value;
-slider.oninput = function(){
-  output.innerHTML = this.value;
-}
+// output.innerHTML = slider.value;
+// slider.oninput = function(){
+//   output.innerHTML = this.value;
+// }
 
-slider.addEventListener("mousemove", function(){
-  var value = slider.value;
-})
+// slider.addEventListener("mousemove", function(){
+//   let value = slider.value;
+// })
