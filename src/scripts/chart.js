182e3fdf25ee2d88f35e3f139ca9e5dd7e5648df
angrysoftwareengineer88@gmail.com <raylan.liang@gmail.com>
2021-01-13 13:02:18 -0500

    event listener test

--- src/scripts/chart.js
+++ src/scripts/chart.js
@@ -1,13 +1,15 @@
 import Chart from 'chart.js';
+import { slideValue, inputSlider } from './slider.js';
 
-let ctx = document.getElementById('myChart');
-let myChart = new Chart(ctx, {
+const ctx = document.getElementById('myChart');
+
+const myChart = new Chart(ctx, {
   type: 'pie',
   data: {
     labels: ['sleep', 'commute', 'work', 'personal time'],
     datasets: [{
       label: '# of hours',
-      data: [7, 2, 8, 10],
+      data: [8, 2, 8, 6],
       backgroundColor:[
         '#f1c40f',
         '#e67e22',
@@ -21,4 +23,27 @@ let myChart = new Chart(ctx, {
     maintainAspectRatio: false
   }
 });
+
+function addData(chart, label, data) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
+}
+
+// function updateConfigByMutating(chart) {
+//   chart.options.title.text = 'new title';
+//   chart.update();
+// }
+
+function updateData() {
+  let newData = [];
+
+  debugger;
+  newData.push(inputSlider.value);
+
+  myChart.data.datasets[0].data = newData;
+  myChart.update();
+}
 
--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,15 +1,39 @@
-const slideValue = document.querySelector("span");
-const inputSlider = document.querySelector("input");
+const { data } = require("autoprefixer");
+
+export const slideValue = document.querySelector("span");
+export const inputSlider = document.getElementById("myRange");
+export const inputSliderValue = document.getElementById("myRange").value;
+export const slider = document.querySelector("input[type='range']");
+
 // const inputSlider = document.getElementsByClassName("question");
+  debugger;
 
 inputSlider.oninput = (() => {
-  let value = inputSlider.value;
+  debugger;
+  // dataArray = [];
+  // dataArray.push(inputSlider.value);
+  console.log(inputSlider);
+  console.log(inputSlider.value);
   slideValue.textContent = value;
   // slideValue.style.left = (value/2) + "%";
   slideValue.style.left = value*4 + "%";
-  slideValue.classList.add("show");
+  // slideValue.classList.add("show");
 });
+  debugger;
 
-inputSlider.onblur = (() => {
-  slideValue.classList.remove("show");
+// inputSlider.onblur = (() => {
+//   slideValue.classList.remove("show");
+// });
+
+
+// NEED EVENT LISTENER TO GET UPDATE INPUT SLIDER VALUE
+// data set array has to be removed and readded, 
+// there's no direct update for chartJs
+
+// inputSlider.addEventListener('input', updateData() {
+//   myRange.value = input.value;
+//   console.log(input.value);
+// });
+inputSlider.addEventListener('input', e => {
+  console.log(e);
 });
